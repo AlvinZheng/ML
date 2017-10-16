e2a65a626f8dc7cfb8c26da2a40e3024b62b62e2
qingzhou.zheng <qingzhou.zheng@ck-telecom.com>
2017-10-16 10:12:22 +0800

    Initial Commit

---
+++ Gradient Desend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gradient Desend
+//
+//  Created by alvin zheng on 17/10/16.
+//  Copyright © 2017年 alvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
